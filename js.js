35f8087723f689a8d2e854cfdf84f07b3f50e528
Martin Kleppe <kleppe@ubilabs.net>
2011-10-18 11:10:26 +0200

    Add link to the 140byt.es source of the JS logo.

--- js.js
+++ js.js
@@ -1,1 +1,1 @@
-function(a,b,c){for(a=12;a--;c=[c]+(4095^[,396,82,66,76,80,76][a]).toString(2)+2);for(a in c)b=[b]+" ■\n"[c[a]];return b}
+function(a,b,c){for(a=12;a--;c=[c]+(4095^[,396,82,66,76,80,76][a]).toString(2)+2);for(a in c)b=[b]+" ■\n"[c[a]];return b} // j.mp/140js
